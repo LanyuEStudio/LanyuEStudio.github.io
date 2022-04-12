cbf9f067969a28812bcdb1bc8ed778f2afc161b0
AwesomeM4n <lonchezas@hotmail.com>
2022-04-12 12:53:35 +0200

    Actualización de enlaces

--- js/lanyu.js
+++ js/lanyu.js
@@ -129,15 +129,15 @@ const tabs = [
   { id: 'development',
     iconClass: 'fa fa-code',
     left: [
-      { name: 'Web Personalizada', href: 'https://fatimahispania.com', img: { url: 'imgs/web.webp', alt: 'Inicio de fatimahispania.com' }, desc: 'Tu web profesional sin preocupaciones.<br/>Visita nuestro trabajo más reciente para bailarinas' },
+      { name: 'Web Personalizada', href: 'https://fatima.lanyu.es', img: { url: 'imgs/web.webp', alt: 'Inicio de fatimahispania.com' }, desc: 'Tu web profesional sin preocupaciones.<br/>Visita nuestro trabajo más reciente para bailarinas' },
       //{ name: 'Videojuegos', href: 'https://lanyu-estudio.itch.io/', img: { url: 'imgs/lanyu-itchio.webp', alt: 'Home de itch.io' }, desc: 'Nos encanta jugar y hacer juegos. Visita nuestra web itch.io y juega' },
       { name: 'Compromiso Open Source', href: 'https://github.com/LanyuEStudio', img: { url: 'imgs/opensource.webp', alt: 'Logo de open source initiative y github social coding' }, desc: 'Explotamos y desarrollamos código abierto (Open Source) <span class="d-none d-sm-inline">para mejorar la velocidad, mantenimiento y precio de desarrollo</span>' },
-      { name: 'API REST', href: 'https://explore.postman.com/43dKUrBDxFN97m', img: { url: 'imgs/postman.webp', alt: 'Documentación APIs Lanyu EStudio en Postman' }, desc: 'Prueba nuestra API RESTful (nivel 3 HATEOAS) sobre Datos Deportivos<span class="d-none d-sm-inline">. Puedes ver las llamadas permitidas en la documentación</span>' }
+      { name: 'API REST', href: 'https://documenter.getpostman.com/view/9800655/Szme3dFP', img: { url: 'imgs/postman.webp', alt: 'Documentación APIs Lanyu EStudio en Postman' }, desc: 'Prueba nuestra API RESTful (nivel 3 HATEOAS) sobre Datos Deportivos<span class="d-none d-sm-inline">. Puedes ver las llamadas permitidas en la documentación</span>' }
     ],
     right: [
       { name: 'Aplicaciones Web', href: 'https://pockets.lanyu.es', img: { url: 'imgs/pockets.webp', alt: 'Ejemplo de monederos en pockets.lanyu.es' }, desc: 'Aplicaciones tanto de negocio como personales.<br/>Prueba nuestra webapp de monederos para enseñar a tus hijos con un sistema de recompensas' },
       { name: 'Infraestructura Cloud', href: 'https://printoonline.com/', img: { url: 'imgs/printoonline.webp', alt: 'Inicio de sitio web PrintOOnline' }, desc: 'Despliegues en la nube<span class="d-none d-sm-inline">, como nuestro desarrollo para Boxer Publicidad, desplegado</span> en Firebase' },
-      { name: 'Integración con Redes Sociales', href: 'https://nomasabusos.es', img: { url: 'imgs/nomasabusos.webp', alt: 'Inicio de nomasabusos.es' }, desc: 'Crea una "Landing page" de entrada a todas tus redes, rápida y vistosa al compartir en ellas y con tu nombre de dominio personalizado' },
+      { name: 'Integración con Redes Sociales', href: 'https://nomasabusos.github.io/', img: { url: 'imgs/nomasabusos.webp', alt: 'Inicio de nomasabusos.es' }, desc: 'Crea una "Landing page" de entrada a todas tus redes, rápida y vistosa al compartir en ellas y con tu nombre de dominio personalizado' },
       //{ name: 'Formación', href: 'https://hijosdelspectrum.blogspot.com/', img: { url: 'imgs/blog.webp', alt: 'Imagen de blog hijosdelspectrum.com' }, desc: 'Experiencia docente a nivel postgrado. Ofrecemos cursos gratuitos en nuestros blogs y canal de youtube' }
     ]
   },
@@ -155,13 +155,13 @@ const tabs = [
   { id: 'learning',
     iconClass: 'fa fa-graduation-cap',
     left: [
-      { name: 'Formación personalizada', href: 'mailto:info@lanyu.es', img: { url: 'imgs/classroom.webp', alt: 'Aula con alumnos' }, desc: 'Para particulares y empresas tanto presenciales como online debido a COVID. <strong>Solicita tu clase con profesor</strong> especializado y dedicado (no sólo entrega de contenidos)' },
-      { name: 'Java Fundamentos (JAZZ)', href: 'https://www.hijosdelspectrum.com/p/curso-java-de-zero-zen-jazz-descripcion.html', img: { url: 'imgs/java.webp', alt: 'Logotipo Java' }, desc: 'Aprende el lenguaje de iniciación a la programación por excelencia hasta alcanzar resultados empresariales' },
-      { name: 'Spring Framework', href: 'https://www.hijosdelspectrum.com/p/spring-framework-crear-una-api.html', img: { url: 'imgs/spring.webp', alt: 'Logotipo Spring Framework' }, desc: 'Aprende el framework de desarrollo de servicios web más extendido a nivel profesional' }
+      { name: 'Formación personalizada', href: 'mailto:info@lanyu.es', img: { url: 'imgs/classroom.webp', alt: 'Aula con alumnos' }, desc: 'Para particulares y empresas tanto presenciales como online. <strong>Solicita tu clase con profesor</strong> especializado y dedicado (no sólo entrega de contenidos)' },
+      { name: 'Java Fundamentos (JAZZ)', href: 'https://hijosdelspectrum.blogspot.com/p/curso-java-de-zero-zen-jazz-descripcion.html', img: { url: 'imgs/java.webp', alt: 'Logotipo Java' }, desc: 'Aprende el lenguaje de iniciación a la programación por excelencia hasta alcanzar resultados empresariales' },
+      { name: 'Spring Framework', href: 'https://hijosdelspectrum.blogspot.com/p/spring-framework-crear-una-api.html', img: { url: 'imgs/spring.webp', alt: 'Logotipo Spring Framework' }, desc: 'Aprende el framework de desarrollo de servicios web más extendido a nivel profesional' }
     ],
     right: [
-      { name: 'JavaScript', href: 'https://www.hijosdelspectrum.com/p/blog-page.html', img: { url: 'imgs/js.webp', alt: 'Logotipo JavaScript' }, desc: 'Aprende lo necesario para poder crear aplicaciones con el lenguaje nativo de la web' },
-      { name: 'Trucos variados', href: 'https://www.hijosdelspectrum.com/2012/09/css-botones-redondeados-con-brillo.html', img: { url: 'https://cdn.pixabay.com/photo/2020/03/05/17/35/tips-4905013_960_720.jpg', alt: 'Tips and Tricks' }, desc: 'Trucos variados sobre programación, software, herramientas, etc...<br/><strong>Permanece conectado!</strong>' }
+      { name: 'JavaScript', href: 'https://hijosdelspectrum.blogspot.com/p/blog-page.html', img: { url: 'imgs/js.webp', alt: 'Logotipo JavaScript' }, desc: 'Aprende lo necesario para poder crear aplicaciones con el lenguaje nativo de la web' },
+      { name: 'Trucos variados', href: 'https://hijosdelspectrum.blogspot.com/2012/09/css-botones-redondeados-con-brillo.html', img: { url: 'https://cdn.pixabay.com/photo/2020/03/05/17/35/tips-4905013_960_720.jpg', alt: 'Tips and Tricks' }, desc: 'Trucos variados sobre programación, software, herramientas, etc...<br/><strong>Permanece conectado!</strong>' }
     ]
   }
 ];
@@ -170,7 +170,7 @@ const services = [
   { title: 'Web Services', header: 'Microservicios & API REST', class: 'fa fa-code', body: 'Realizamos <a href="https://es.wikipedia.org/wiki/Transferencia_de_Estado_Representacional">API REST</a> nivel 3 (<a href="https://en.wikipedia.org/wiki/HATEOAS">HATEOAS</a>) implementado habitualmente usando el nivel de abstracción más alto de <a href="https://spring.io/projects/spring-framework">Spring Framework</a> sobre Spring Data. Esto proporciona la mayor flexibilidad, escalabilidad y reutilización en una infraestructura ágil basada en <a href="https://www.redhat.com/es/topics/microservices">microservicios</a>.<br/><strong>¡Rompe tu monolito y convierte tu empresa realmente a procesos de negocio!<strong>' },
   { title: 'Videojuegos', header: 'La cultura que más crece y engancha', class: 'fa fa-gamepad', body: 'Apoyándonos en proyectos con grandes comunidades de software libre como <a href="https://libgdx.badlogicgames.com/">libgdx</a>, desarrollamos librerías de código abierto para crear rápidamente videojuegos que vayan aportando capas reutilizables para videojuegos más complicados (ECS, networking, diálogos, etc...)' },
   { title: 'Websites', header: 'Páginas web personalizadas asequibles', class: 'fa fa-code', body: 'Si no quieres tener otro clon de página web usando un servicio gratuito como <a href="https://es.wix.com/wix">wix</a> nos puedes pedir tu página web personalizada para tener un acabado profesional que alcance las mejores métricas (SEO, rendimiento, buenas prácticas,...). Esto hará que esté mejor posicionada y atraiga más usuarios. Además, te proporcionamos acceso a métricas para que puedas adaptarte a lo que quieren tus visitantes.' },
-  { title: 'Formación', header: 'Larga experiencia en formación', class: 'fa fa-graduation-cap', body: 'Tenemos mucha experiencia formando personal tanto en la administración pública como en empresa. Prueba los <a href="https://www.hijosdelspectrum.com/p/blog-page.html"><strong>cursos online gratuitos</strong> que compartimos en nuestro blog</a> y contrata cursos especializados particularmente o para tus empleados. <strong><u>El mejor contenido gratuito en español</u></strong>.' },
+  { title: 'Formación', header: 'Larga experiencia en formación', class: 'fa fa-graduation-cap', body: 'Tenemos mucha experiencia formando personal tanto en la administración pública como en empresa. Prueba los <a href="https://hijosdelspectrum.blogspot.com/p/blog-page.html"><strong>cursos online gratuitos</strong> que compartimos en nuestro blog</a> y contrata cursos especializados particularmente o para tus empleados. <strong><u>El mejor contenido gratuito en español</u></strong>.' },
   { title: 'Open Source', header: 'Amamos la mentalidad Open Source', class: 'fa fa-code', body: 'Liberamos código (en <a href="https://github.com/LanyuEStudio">nuestro repositorio de github</a>) para que, igualmente que nos aprovechamos del trabajo de otros para abaratar costes de desarrollo y mejorar el mantenimiento, podamos retornar valor a la comunidad. También hacemos donaciones a los mejores proyectos (¡no sólo de código vive el hombre!) y <a href="https://ko-fi.com/lanyuestudio">aceptamos micromecenazgo</a> para ello.' }
 ];
 
@@ -178,13 +178,13 @@ const references = [
   { name: 'Julio Fuentes-Pila', image: { url: 'imgs/boxer-publicidad.webp', description: 'Boxer publicidad'}, category: 'Propietario', content: 'Entendió el proyecto desde el primer momento e incluso ha realizado aportaciones muy importantes al mismo. Lo más llamativo de su trabajo es su forma de rentabilizar el tiempo de programación. Sin duda seguiré contando con él.<br/><a href="https://printoonline.com/">ver proyecto</a>' },
   { name: 'blue lander games', image: { url: 'https://img.itch.zone/aW1nLzc1NDEzMjgucG5n/80x80%23/aD8nSN.png', description: 'blue lander games'}, category: 'itch.io user', content: "I discovered MYA in the bundle for racial justice, just wanted to say this is a really cool project! The world needs a visual novel maker that's as easy to use as Twine, this has a lot of potential. Hope you're able to keep working on it!<br/><a href=\"https://lanyu-estudio.itch.io/make-your-adventure\">ver juego</a>" },
   { name: 'Gonzalo Kouyoumdjian', image: { url: 'https://media-exp1.licdn.com/dms/image/C4D03AQFRHCynDEZZcQ/profile-displayphoto-shrink_200_200/0/1584286811833?e=1654732800&v=beta&t=3BUDcJxo0600O-rH2T-Vwvrlp1wSTnepYyCv_Q7lOgA', description: 'Gonzalo Kouyoumdjian'}, category: 'MBA Finance, MSc Industrial Engineering', content: 'Ismael ha desarrollado el proyecto con mucha rapidez. Al mismo tiempo ha entendido el problema presentado y encontrado las soluciones.' },
-  { name: 'Patty', image: { url: 'imgs/anonimo.webp', description: 'sin imagen'}, category: 'Alumno curso JavaScript', content: 'Muy buena tu pagina :)<br/><a href="https://www.hijosdelspectrum.com/2017/10/javascript-dom.html">ver post</a>' }
+  { name: 'Patty', image: { url: 'imgs/anonimo.webp', description: 'sin imagen'}, category: 'Alumno curso JavaScript', content: 'Muy buena tu pagina :)<br/><a href="https://hijosdelspectrum.blogspot.com/2017/10/javascript-dom.html">ver post</a>' }
 ];
 
 const rrss = [
   { name: 'facebook', class: 'fa fa-facebook', url: 'https://fb.me/LanyuEStudio' },
   { name: 'twitter', class: 'fa fa-twitter', url: 'https://twitter.com/LanyuEStudio' },
-  { name: 'Blogger', class: 'fa fa-rss', url: 'https://hijosdelspectrum.com/' },
+  { name: 'Blogger', class: 'fa fa-rss', url: 'https://hijosdelspectrum.blogspot.com/' },
   { name: 'YouTube', class: 'fa fa-youtube', url: 'https://www.youtube.com/channel/UCM02UBS6SkPfk_fVg1k_9gg' },
   { name: 'GitHub', class: 'fa fa-github', url: 'https://github.com/LanyuEStudio' }
 ];
